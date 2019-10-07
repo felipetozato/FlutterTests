c5f5c3bd2874a89fa25ef156309ddaadb46cb0ec
Felipe Cantagalli <felipe.cantagalli@gmail.com>
2019-10-06 23:18:57 -0300

    initial files for project

---
+++ android/app/src/main/kotlin/com/felipe/test/avocado_test/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.felipe.test.avocado_test
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
